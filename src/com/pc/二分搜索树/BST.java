9f0559f42de45f600e8e010d46126f19b739b568
EvanZhou666 <1043056891@qq.com>
2019-03-29 17:14:54 +0800

    插入节点实现方式2、查找最小元素、是否包含某个元素

--- src/com/pc/二分搜索树/BST.java
+++ src/com/pc/二分搜索树/BST.java
@@ -22,7 +22,7 @@ public class BST<E extends Comparable<E>> {
     }
 
     /**
-     * 递归插入节点
+     * 递归插入节点 实现方式一 比较挫...
      * @param e
      */
     public void add(E e) {
@@ -70,6 +70,56 @@ public class BST<E extends Comparable<E>> {
         }
     }
 
+//    二分搜索树中插入节点 实现方式2
+    public void add2(E e){
+        root = add2(new Node(e),root);
+    }
+
+//    在以root为根的子树上插入节点，并且返回新的子树（一个Node本身就可以看做是一棵树）
+    private Node add2(Node insert ,Node root){
+        if (root==null){
+            return insert;
+        }
+        if (insert.data.compareTo(root.data)>0){
+            //挂接新的子树到右节点上
+            root.right = add2(insert,root.right);
+        } else {
+            //挂接新的子树到左节点上
+            root.left = add2(insert,root.left);
+        }
+        return root;
+    }
+
+//    查找元素
+    public boolean contains(E e) {
+       return findE(e,root);
+    }
+
+    public boolean findE(E e,Node root) {
+        if (root ==null) {
+            return false;
+        }
+        if (e.compareTo(root.data)>0) {
+            return findE(e,root.right);
+        } else if(e.compareTo(root.data)<0) {
+            return findE(e,root.left);
+        } else {
+            return true;
+        }
+    }
+
+//    查找最小的元素 因为最最小的元素一定在左子树的叶子节点上，因此递归的终止条件是root.left==null
+    public E findMix (){
+        if (root ==null) { throw new UnsupportedOperationException("不支持改操作"); }
+        return dofindMix(root).data;
+    }
+
+    private Node dofindMix(Node root) {
+        if (root.left==null){
+            return root;
+        }
+        return dofindMix(root.left);
+    }
     private class Node {
         private E data;
         Node left,right;
@@ -81,9 +131,20 @@ public class BST<E extends Comparable<E>> {
 
     public static void main(String[] args) {
         BST<Integer> ebst = new BST<>();
-        ebst.add(8);
+      /*  ebst.add(8);
         ebst.add(7);
         ebst.add(6);
-//        ebst.add(10);
+        ebst.add(100);
+        ebst.add(88);*/
+        ebst.add2(8);
+        ebst.add2(7);
+        ebst.add2(6);
+        ebst.add2(100);
+        ebst.add2(88);
+        System.out.println(ebst.contains(100));
+        System.out.println(ebst.contains(101));
+        System.out.println(ebst.contains(88));
+        System.out.println(String.format("最小的节点是%s",ebst.findMix()));
+
     }
 }
