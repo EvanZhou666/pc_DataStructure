47df56dc099b0da998cb212b407c84a856179178
EvanZhou666 <1043056891@qq.com>
2019-04-01 11:41:00 +0800

    二叉堆bug修复、优化初始化heapfy操作

--- src/com/pc/堆/BinaryHeap.java
+++ src/com/pc/堆/BinaryHeap.java
@@ -1,5 +1,6 @@
 package com.pc.堆;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
@@ -24,16 +25,40 @@ public class BinaryHeap<E extends Comparable<E>> {
     }
 
     public BinaryHeap(E[] items){
+        data = (E[])new Comparable[items.length+front];
+        System.arraycopy(items,0,data,front,items.length);
+        System.out.println(Arrays.asList(data));
+        currentSize = items.length;
+//        这种方法效率不高
+        /*
         this(DEFAULT_CAPASITY);
         for (E item : items) {
             add(item);
-        }
+        }*/
+        buildHeap(currentSize/2);
+    }
 
+    /**
+     * 从第一个非叶子节点执行“下沉”操作
+     * @param index
+     */
+    private void buildHeap(int index){
+        while (index!=0){
+            siftDown(index);
+            index--;
+        }
+    }
+
+//    交换a，b元素
+    private void swap(int a,int b){
+        E temp = data[b];
+        data[b] = data[a];
+        data[a] = temp;
     }
 
     public void add(E e){
         if (currentSize+1>capasity){
-            enlarge(capasity*2+1);
+            enlarge(capasity*2+front);
         }
         data[++currentSize] = e;
         siftUp(currentSize,e);
@@ -48,8 +73,7 @@ public class BinaryHeap<E extends Comparable<E>> {
         int parent = currentSize/2;
         while (parent!=0){
             if (e.compareTo((E) data[parent])<0){
-                data[currentSize] = data[parent];
-                data[parent] = e;
+                swap(currentSize,parent);
                 parent/=2;
             } else {
                 break;
@@ -65,6 +89,7 @@ public class BinaryHeap<E extends Comparable<E>> {
 //    将最后一个元素赋给根节点，根节点中的元素和左右孩子节点中最小的一个比较，如果比最小的大，则它们交换位置后继续与左右孩子比较，以此类推一直下沉...
     public E deleteMin(){
         E min = data[front];
+        data[front] = data[currentSize];
         siftDown(front);
         data[currentSize] = null;
         currentSize--;
@@ -72,7 +97,7 @@ public class BinaryHeap<E extends Comparable<E>> {
     }
 
     private int siftDown(int front) {
-        data[front] = data[currentSize];
+//        data[front] = data[currentSize];
         int left = 2 * front <= currentSize ? 2 * front:-1;
         int right = 2 * front+1 <= currentSize ? 2 * front+1:-1;
         if (left ==-1){
@@ -80,9 +105,7 @@ public class BinaryHeap<E extends Comparable<E>> {
         }
         int minIndex = min(left,right);//返回左右孩子较小的节点索引
         if (data[front].compareTo(data[minIndex])>0){
-            E swap = data[minIndex];
-            data[minIndex] = data[front];
-            data[front] = swap;
+            swap(front,minIndex);
             siftDown(minIndex);
         }
         return front;
@@ -153,23 +176,39 @@ public class BinaryHeap<E extends Comparable<E>> {
             }
         }
     }
+
+    public static Integer[] testHeapfy(int nums ){
+        Integer[] rands = new Integer[nums];
+        for(int i=0;i<nums;i++){
+            int rand = new Random().nextInt(1000)+1;
+            rands[i]=rand;
+        }
+        return rands;
+    }
+
     public static void main(String[] args) {
 
-        BinaryHeap<Integer> eBinaryHeap = new BinaryHeap<>();
-       for(int i=0;i<500;i++){
-            int rand = new Random().nextInt(500)+1;
+     /*   BinaryHeap<Integer> eBinaryHeap = new BinaryHeap<>();
+        Integer[] rands = testHeapfy(10);
+        System.out.println(Arrays.toString(rands));
+        for (Integer rand : rands) {
             eBinaryHeap.add(rand);
-//           eBinaryHeap.testHasError();
         }
+//           eBinaryHeap.testHasError();
          eBinaryHeap.levelOrder();
-        for (int i =0;i<500;i++){
+        for (int i =0;i<10;i++){
             System.out.println();
             eBinaryHeap.deleteMin();
             eBinaryHeap.levelOrder();
             eBinaryHeap.testHasError();
-        }
+        }*/
      /*  Integer[] nums = new Integer[]{17,43,33,94,56,76,48,71};
         BinaryHeap<Integer> eBinaryHeap = new BinaryHeap<>(nums);
         eBinaryHeap.levelOrder();*/
+//        new Integer[]{88, 400, 160, 153, 485}
+//        测试构建堆的算法
+        BinaryHeap<Integer> eBinaryHeap = new BinaryHeap<>(testHeapfy(100));
+        eBinaryHeap.levelOrder();
+        eBinaryHeap.testHasError();
     }
 }
