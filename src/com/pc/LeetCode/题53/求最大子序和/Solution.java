1c81d7d1f955730a40b956f4fd5665fcee55c5ca
zhouzixiang <zhouzixiang@gimc.cn>
2020-09-01 23:20:08 +0800

    提交平衡二叉树解法

---
+++ src/com/pc/LeetCode/题53/求最大子序和/Solution.java
@@ -0,0 +1,32 @@
+package com.pc.LeetCode.题53.求最大子序和;
+
+/**
+ *
+ *@author EvanZhou
+ *@since 2020/9/1
+ */
+public class Solution {
+
+	public int maxSubArray(int[] nums) {
+		int max = nums.length;
+		int i = 0;
+		int j = nums.length - 1;
+		while ((nums[i] < 0 || nums[j] < 0) && j > i) {
+			max--;
+			if (nums[i] > nums[j]) {
+				j--;
+			} else if (nums[i] < nums[j]) {
+				i++;
+			} else {
+				i++;
+				j--;
+			}
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		System.out.println(solution.maxSubArray(new int[]{21,-2222,-999999,111111,11}));
+	}
+}
