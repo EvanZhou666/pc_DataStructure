4b30921bdc81dd14e8f067915f01c934de98028d
EvanZhou <zhouzixiang@lizhi.fm>
2021-03-11 15:06:51 +0800

    二叉树的序列化和反序列化

---
+++ src/com/pc/LeetCode/题449/序列化和反序列化二叉搜索树/Codec.java
@@ -0,0 +1,19 @@
+package com.pc.LeetCode.题449.序列化和反序列化二叉搜索树;
+
+import com.pc.LeetCode.common.TreeNode;
+
+/**
+ *
+ */
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+
+
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
+}
