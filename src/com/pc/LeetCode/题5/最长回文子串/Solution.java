aaa8914d302bbb8ce769c77165bb8696bf6015a4
zhouzixiang <zhouzixiang@lizhi.fm>
2022-05-20 23:50:29 +0800

    feat：回文串系列

--- src/com/pc/LeetCode/题5/最长回文子串/Solution.java
+++ src/com/pc/LeetCode/题5/最长回文子串/Solution.java
@@ -1,60 +1,50 @@
 package com.pc.LeetCode.题5.最长回文子串;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
- *
- *@author Evan
- *@since 2020/9/7 14:39
+ * @author Evan
+ * @since 2020/9/7 14:39
  */
 public class Solution {
-	public static String longestPalindrome(String s) {
-		// 正序
-		List<Character> pos = new ArrayList();
-		for (char po : s.toCharArray()) {
-			pos.add(po);
-		}
+    public static String longestPalindrome(String s) {
+        char[] chars = s.toCharArray();
+        String maxStr = "";
+        for (int i = 0; i < chars.length; i++) {
+            // 以该节点和该节点的下一个节点之间的点（虚拟的点，实际不存在的情况），不断向两边延伸，生成的偶数回文串
+            String s1 = extend(i, i, s, chars);
+            // 以该节点为中心，不断向两边延伸，生成的奇数回文串
+            String s2 = extend(i, i + 1, s, chars);
+            if (Math.max(s1.length(), s2.length()) > maxStr.length()) {
+                if (s1.length()>s2.length()) {
+                    maxStr = s1;
+                } else {
+                    maxStr = s2;
+                }
+            }
+        }
+        return maxStr;
+    }
 
-		List<Character> nav = new ArrayList<>(pos.size());
-		char[] chars = s.toCharArray();
-		for (int i = chars.length ; i > 0;) {
-			nav.add(chars[--i]);
-		}
+    /**
+     * 伸展回文串
+     * @param left
+     * @param right
+     * @param s
+     * @param chars
+     * @return
+     */
+    private static String extend(int left, int right, String s, char[] chars) {
+        while (left >= 0 && right < s.length() && chars[left] == chars[right]) {
+            left--;
+            right++;
+        }
+        return s.substring(left+1, right);
+    }
 
-		System.out.println(pos);
-		System.out.println(nav);
+    public static void main(String[] args) {
+        String s = "adad";
 
-		int start = 0;
-		int maxLength = 0;
-		int length = 0;
-		for (int i =0;i<pos.size();) {
-			if (pos.get(i) == nav.get(i)) {
-				if (start == 0) {
-					start = i;
-				}
-				i++;
-				length ++;
-			} else {
-				if (length>maxLength) {
-					maxLength = length;
-					length = 0;
-				} else {
-					i++;
-					start = i;
-				}
-			}
-		}
-
-		return s.substring(start,start+length);
-
-	}
-
-	public static void main(String[] args) {
-		String s = "babad";
-
-		String s1 = longestPalindrome(s);
-		System.out.println(s1);
-	}
+        String s1 = longestPalindrome(s);
+        System.out.println(s1);
+    }
 }
