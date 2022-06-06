30c83623c66b0e547cfc4683644f9fc7afe6fc44
zhouzixiang <zhouzixiang@lizhi.fm>
2022-06-06 08:39:51 +0800

    feat：【二分搜索系列】-固定滑动窗口+Equals实现indexOf，代码细节调整，套用二分搜索的模板

--- src/com/pc/LeetCode/题35/Solution.java
+++ src/com/pc/LeetCode/题35/Solution.java
@@ -16,10 +16,10 @@ public class Solution {
     public int searchInsert(int[] nums, int target) {
         int l = 0;
         int r = nums.length -1 ;
-        while (l < r) {
+        while (l <= r) {
             int mid = (l + r) / 2;
             if (target < nums[mid]) {
-                r = mid;
+                r = mid - 1;
             } else if (target > nums[mid]) {
                 l = mid + 1;
             } else {
