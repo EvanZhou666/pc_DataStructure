9979bc7090788008c5a03a31a8feb116a2c54af0
zhouzixiang <zhouzixiang@lizhi.fm>
2022-06-07 23:37:42 +0800

    feat：【贪心算法系列】- 跳跃游戏II

---
+++ src/com/pc/LeetCode/题45/Solution.java
@@ -0,0 +1,42 @@
+package com.pc.LeetCode.题45;
+
+/**
+ * 45. 跳跃游戏 II
+ * 给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ *
+ * 假设你总是可以到达数组的最后一个位置。
+ *
+ * 输入: nums = [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/jump-game-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    /**
+     * 书本上的答案
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int end = 0, farthest = 0;
+        int jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(nums[i] + i, farthest);
+            if (end == i) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+}
