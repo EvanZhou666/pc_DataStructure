cb1ebe447197633b004ab17ac2776a10e89571d0
EvanZhou666 <1043056891@qq.com>
2022-08-05 23:22:59 +0800

    feat:【动态规划系列】-最长连续序列。动态规划解法，暂时不是很理解

---
+++ src/com/pc/LeetCode/题128/Solution03.java
@@ -0,0 +1,68 @@
+package com.pc.LeetCode.题128;
+
+import com.pc.LeetCode.common.Assert;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * 128. 最长连续序列
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ *
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 示例 2：
+ *
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ *
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
+// 动态规划解法 -优化
+public class Solution03 {
+
+    public int longestConsecutive(int[] nums) {
+        // key表示num，value表示num所在连续区间的长度
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int num : nums) {
+            // 当map中不包含num，也就是num第一次出现
+            if (!map.containsKey(num)) {
+                // left为num-1所在连续区间的长度，更进一步理解为：左连续区间的长度
+                int left = map.getOrDefault(num - 1, 0);
+                // right为num+1所在连续区间的长度，更进一步理解为：右连续区间的长度
+                int right = map.getOrDefault(num + 1, 0);
+                // 当前连续区间的总长度
+                int curLen = left + right + 1;
+                ans = Math.max(ans, curLen);
+                // 将num加入map中，表示已经遍历过该值。其对应的value可以为任意值。
+                map.put(num, -1);
+                // 更新当前连续区间左边界和右边界对应的区间长度
+                map.put(num - left, curLen);
+                map.put(num + right, curLen);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution03 solution03 = new Solution03();
+        int ans = solution03.longestConsecutive(new int[]{100,4,200,1,3,2});
+        Assert.assertEquals(ans, 4);
+
+    }
+}
