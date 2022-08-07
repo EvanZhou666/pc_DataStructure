964a737e4e3fc6a2cc263caa051f9d30f8ebdc10
EvanZhou666 <1043056891@qq.com>
2022-08-07 20:21:31 +0800

    feat:工具类

--- src/main/java/com/pc/LeetCode/common/Assert.java
+++ src/main/java/com/pc/LeetCode/common/Assert.java
@@ -1,5 +1,6 @@
 package com.pc.LeetCode.common;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Assert {
@@ -35,6 +36,15 @@ public class Assert {
     public static boolean assertEquals(String realValue, String excepted) {
         if (!Objects.equals(realValue,excepted)) {
             throw new IllegalArgumentException("The value is not Equals.excepted is "+ excepted +" but realvalue is "+realValue);
+        }
+        return true;
+    }
+
+    public static boolean assertEquals(char[][] realValue, char[][] excepted) {
+        for (int i = 0; i < realValue.length; i++) {
+            if (!Arrays.equals(realValue[i], excepted[i])) {
+                throw new IllegalArgumentException("第" + i + "行 The value is not Equals.excepted is "+ Arrays.toString(excepted[i]) +" but realvalue is "+ Arrays.toString(realValue[i]));
+            }
         }
         return true;
     }
---
+++ src/main/java/com/pc/LeetCode/common/Uitls.java
@@ -0,0 +1,59 @@
+package com.pc.LeetCode.common;
+
+import com.alibaba.fastjson.JSONArray;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Uitls {
+
+    /**
+     * 打印数组以及双指针，方便调试用
+     *
+     * @param nums
+     * @param l
+     * @param r
+     */
+    public static void printArray(char[] nums, int l, int r) {
+        if (r >= nums.length) {
+            System.out.println(Arrays.toString(nums) + " l=" + l + " r=nil");
+        } else {
+            System.out.println(Arrays.toString(nums) + " l=" + l + " r=" + r);
+        }
+    }
+
+    public static void printArray(int[] nums, int l, int r) {
+        if (r >= nums.length) {
+            System.out.println(Arrays.toString(nums) + " l=" + l + " r=nil");
+        } else {
+            System.out.println(Arrays.toString(nums) + " l=" + l + " r=" + r);
+        }
+    }
+
+    public static char[][] convert(String inputString) {
+        Character[] CC = new Character[0];
+        List<? extends Character[]> jsonArray = JSONArray.parseArray(inputString, CC.getClass());
+
+        char[][] input = new char[jsonArray.size()][jsonArray.get(0).length];
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            for (int j = 0; j < jsonArray.get(i).length; j++) {
+                Character[] character = jsonArray.get(i);
+                input[i][j] = character[j];
+            }
+
+        }
+        return input;
+    }
+    public static void printBoard(char[][] board, String message) {
+        System.out.println("==========="+message+"========");
+        for (int i = 0; i < board.length; i++) {
+            if (i < 10) {
+                System.out.println(" " + i + ":" + Arrays.toString(board[i]));
+            } else {
+                System.out.println(i + ":" + Arrays.toString(board[i]));
+            }
+        }
+    }
+
+}
