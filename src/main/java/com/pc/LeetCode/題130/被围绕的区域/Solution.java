128294dd416b1963c01840a80c17abad0a4863a3
EvanZhou666 <1043056891@qq.com>
2022-08-08 11:11:21 +0800

    refactor:添加题目说明

--- src/main/java/com/pc/LeetCode/題130/被围绕的区域/Solution.java
+++ src/main/java/com/pc/LeetCode/題130/被围绕的区域/Solution.java
@@ -1,10 +1,23 @@
 package com.pc.LeetCode.題130.被围绕的区域;
 
-import com.alibaba.fastjson.JSONArray;
 import com.pc.LeetCode.common.Assert;
 import com.pc.LeetCode.common.Uitls;
 
 import java.util.*;
+
+/**
+ * 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。<br/>
+ * <img src="./img_1.png"></img>
+ * <p>输入：board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]</p>
+ * <p>输出：[["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]</p>
+ * <p>解释：被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。</p>
+ *
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/surrounded-regions
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * </p>
+ */
 class Solution {
 
     /**
@@ -257,7 +270,9 @@ class Solution {
         char[][] board = Uitls.convert(input);
 
         Solution solution = new Solution();
+        Uitls.printBoard(board, "替换前");
         solution.solve(board);
+        Uitls.printBoard(board, "替换后");
         String exceptAns = "[['O','O','X','O','O','X','O','O','O','O','O','O','O','O','X','X','O','X','O','O'],['O','X','O','O','O','O','X','X','O','O','O','X','O','O','X','X','O','O','O','O'],['X','X','X','X','O','O','O','O','O','X','O','O','O','X','X','X','X','X','X','O'],['X','X','O','O','O','O','O','O','O','O','X','O','O','X','X','X','X','X','X','X'],['O','O','O','O','O','X','O','O','O','X','X','X','X','O','X','O','O','O','O','O'],['O','X','X','X','O','O','O','X','O','X','X','X','O','O','X','O','X','X','O','O'],['O','O','O','O','O','O','O','O','X','X','X','O','O','X','X','O','O','O','O','O'],['O','X','X','O','O','O','O','O','X','O','X','X','O','X','X','O','O','X','O','O'],['O','O','X','X','X','O','O','X','O','O','O','O','O','O','O','X','X','X','O','X'],['X','X','O','O','O','X','O','X','O','O','O','X','X','O','O','X','X','X','X','O'],['O','O','O','O','O','O','X','X','X','X','O','O','X','O','X','X','X','X','O','X'],['O','O','X','X','O','O','X','X','X','O','O','X','O','O','X','X','X','X','O','X'],['O','X','O','O','O','O','O','X','O','O','O','O','O','O','X','X','X','O','O','O'],['O','O','X','O','X','O','O','X','X','O','X','X','X','O','O','X','X','O','O','X'],['X','O','X','O','X','O','X','O','O','O','O','O','O','O','X','O','O','X','X','O'],['X','O','X','X','X','O','X','O','O','O','O','O','O','X','O','O','O','O','X','X'],['X','O','O','O','O','X','O','O','O','O','O','O','X','O','O','O','O','O','X','X'],['O','O','O','O','O','O','X','O','O','O','X','O','X','O','X','X','O','X','X','X'],['X','O','O','X','O','O','O','O','O','O','X','O','O','O','O','O','X','X','X','X'],['O','O','O','X','X','O','O','O','O','O','O','O','O','X','O','O','O','X','O','O']]";
         char[][] ex = Uitls.convert(exceptAns);
         Uitls.printBoard(ex, "期望值");
