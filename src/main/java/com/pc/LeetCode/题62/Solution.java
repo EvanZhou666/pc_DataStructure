7a1980e04357b0205b7abf368f8700a8ab5fa641
EvanZhou666 <1043056891@qq.com>
2022-08-23 13:28:50 +0800

    refactor:代码优化

--- src/main/java/com/pc/LeetCode/题62/Solution.java
+++ src/main/java/com/pc/LeetCode/题62/Solution.java
@@ -42,12 +42,9 @@ import com.pc.LeetCode.common.Assert;
  */
 public class Solution {
     public int uniquePaths(int m, int n) {
-
         int[][] dp = new int[m][n];
-        dp[0][0] = 1;
-
-        int top = 0;
-        int left = 0;
+        int top;
+        int left;
 
         // 初始化第0行
         for (int j = 0; j < n; j++) {
@@ -60,20 +57,12 @@ public class Solution {
         }
 
         // 横向遍历
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    continue;
-                }
-
-                if (i - 1 >= 0) {
-                    // dp[i-1][j]当前节点的上邻节点
-                    top = dp[i - 1][j];
-                }
-                if (j - 1 >= 0) {
-                    // 当前节点的左邻节点
-                    left = dp[i][j - 1];
-                }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                // dp[i-1][j]当前节点的上邻节点
+                top = dp[i - 1][j];
+                // 当前节点的左邻节点
+                left = dp[i][j - 1];
                 // dp[i][j] 当前节点  ,
                 dp[i][j] = left + top;
             }
