b18818c1a2982596ab6b55c11ccb86c2c841ed87
EvanZhou666 <1043056891@qq.com>
2022-08-27 12:06:46 +0800

    feat:【动态规划系列】0-1背包问题，使用动态规划求解

--- src/main/java/com/pc/背包问题/PackageZeroOneSolution.java
+++ src/main/java/com/pc/背包问题/PackageZeroOneSolution.java
@@ -1,5 +1,7 @@
 package com.pc.背包问题;
 
+import com.pc.LeetCode.common.Assert;
+
 /**
  * 0-1背包问题
  * 有一个背包，它的容量为C。现在有n种不同的物品，编号为0...n-1，
@@ -8,8 +10,21 @@ package com.pc.背包问题;
  */
 public class PackageZeroOneSolution {
 
+    /**
+     * 计算0-1背包问题，求解在容量为C的情况下，背包所放物品的最大价值。
+     * 定义f(n,C)为在容量为C的情况下，考虑将n个物品放入背包，产生的最大价值。则：
+     * 对于第n个物品有两种策略：
+     * 1.不考虑把第n个物品放入到背包中，直接考虑把前n-1个物品放入背包的产生的价值 = f(n-1,C)
+     * 2.考虑把第n个物品放入到背包中，再考虑把前n-1个物品放入背包的产生的价值，因此这种策略的价值= 第n个物品的价值 + 前n-1个物品的价值 = v[n] + f(n-1, C- weight[n])
+     * 因此。考虑将n个物品放入背包，产生的最大价值 ：f(n,C) = max [ f(n-1,C)、 weight[n] ]
+     * @param weight
+     * @param v
+     * @param C
+     * @return
+     */
     public static int knapsack01(int[] weight, int[] v, int C) {
-        return calMaxValue(weight, v, v.length - 1, C);
+//        return calMaxValueRecursive(weight, v, v.length - 1, C);
+        return calMaxValueDp(weight, v, C);
     }
 
     /**
@@ -19,26 +34,62 @@ public class PackageZeroOneSolution {
      * @param n 物品总数
      * @param c 背包容量，能够承受的物品总重量
      */
-    private static int calMaxValue(int[] weight, int[] v, int n, int c) {
+    private static int calMaxValueRecursive(int[] weight, int[] v, int n, int c) {
         // 终止条件：如果背包容量耗尽或者物品放置完成，
         if (c < 0 || n < 0) {
             return 0;
         }
 
         // 不将第n个物品放入背包的最大价值
-        int res = calMaxValue(weight, v, n-1, c);
+        int res = calMaxValueRecursive(weight, v, n-1, c);
 
         // 将第n个物品放入背包的最大价值
         if (c - weight[n] < 0) { // 背包容量不够放入第n个物品，return
             return res;
         }
-        res = Math.max(res, calMaxValue(weight, v, n-1, c - weight[n]) + v[n]);
+        res = Math.max(res, calMaxValueRecursive(weight, v, n-1, c - weight[n]) + v[n]);
 
         return res;
+    }
+
+    /**
+     * 使用动态规划思想求解<br/>
+     * <img src="0-1背包问题.png"/> <br/>
+     * @param weight
+     * @param v
+     * @param C
+     * @return
+     */
+    private static int calMaxValueDp(int[] weight, int[] v, int C) {
+        int n = weight.length; // 物品数目
+        int[][] dp = new int[n][C+1];
+
+        // 初始化第0行数据
+        for (int j = 0; j < dp[0].length; j++) {
+            if (j >= weight[0]) {
+                dp[0][j] = v[0];
+            }
+        }
+
+        // 遍历物品
+        for (int i = 1; i < n; i++) {
+            // 访问先前背包容量
+            for (int j = 0; j < C + 1; j++) {
+                // 不放入第i个物品时的时候最大价值
+                dp[i][j] = dp[i - 1][j];
+                // 减去第i个物品后的背包容量
+                if (j - weight[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weight[i]] + v[i]);
+                }
+            }
+        }
+
+        return dp[n - 1][C];
     }
 
     public static void main(String[] args) {
         int ans = knapsack01(new int[]{1, 2, 3}, new int[]{6, 10, 12}, 5);
         System.out.println(ans);
+        Assert.assertEquals(ans, 22);
     }
 }
