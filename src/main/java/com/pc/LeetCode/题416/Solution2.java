1d708e8b3bbd9301a8ad3d5af1890fbb8a2698bc
EvanZhou666 <1043056891@qq.com>
2022-08-28 20:10:58 +0800

    doc:注释修改

--- src/main/java/com/pc/LeetCode/题416/Solution2.java
+++ src/main/java/com/pc/LeetCode/题416/Solution2.java
@@ -1,5 +1,22 @@
 package com.pc.LeetCode.题416;
 
+/**
+ * 416. 分割等和子集
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,5,11,5]
+ * 输出：true
+ * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,5]
+ * 输出：false
+ * 解释：数组不能分割成两个元素和相等的子集。
+ */
 public class Solution2 {
 
     public static boolean canPartition(int[] nums) {
@@ -26,9 +43,11 @@ public class Solution2 {
         // 初始化 dp[0][j] 代表[0,0]物品的和是否等于j
         for (int j = 0; j <= C; j++) {
             if (nums[0] == j) {
+//        官方dp[0][0]初始化为true就很难受，我偏要初始化为false
                 dp[0][j] = true;
             }
         }
+
 
         for (int i = 1; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
