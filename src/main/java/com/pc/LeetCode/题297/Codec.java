4c33c4e5b28067c75e49eb85aa1ed999a1aa325b
EvanZhou666 <1043056891@qq.com>
2022-09-02 17:53:31 +0800

    refactor：优化代码结构

--- src/main/java/com/pc/LeetCode/题297/Codec.java
+++ src/main/java/com/pc/LeetCode/题297/Codec.java
@@ -16,6 +16,7 @@ public class Codec {
 
     /**
      * bfs广度优先搜索，注意审题，不要求按照给出的输入格式进行序列话。（官方题解都没按照输入格式进行序列化的！！！！）
+     *
      * @param root
      * @return
      */
@@ -31,26 +32,23 @@ public class Codec {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 Object pop = queue.pop();
-                if (pop instanceof String) {
+                if (pop instanceof String) {// 出队的节点是null，自然也不存在儿子节点
                     sb.append(",").append((String) pop);
                 } else {
+                    // 序列化的时候是把儿子节点丢入到队列里,是因为儿子节点也可以成为父节点，丢入队列是为了取儿子节点方便
                     TreeNode node = (TreeNode) pop;
                     sb.append(",").append(node.val);
-                    if (isLeaf(node)) {
-                        queue.offer("null");
-                        queue.offer("null");
-                    } else {
-                        if (node.left != null) {
-                            queue.offer(node.left);
-                        } else {
-                            queue.offer("null");
-                        }
 
-                        if (node.right != null) {
-                            queue.offer(node.right);
-                        } else {
-                            queue.offer("null");
-                        }
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    } else {
+                        queue.offer("null"); // 入队“null”,标识node的孩子节点不存在.
+                    }
+
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    } else {
+                        queue.offer("null"); // 入队“null”,标识node的孩子节点不存在.
                     }
                 }
             }
@@ -61,12 +59,8 @@ public class Codec {
             preHandleStr = sb.substring(1);
         }
         preHandleStr = "[" + preHandleStr + "]";
-        System.out.println(preHandleStr);
+        System.out.println(preHandleStr); // 注意：本输出不是按照官方的示例进行序列化的。官方自己都没那样处理
         return preHandleStr;
-    }
-
-    private static boolean isLeaf(TreeNode treeNode) {
-        return treeNode != null && treeNode.left == null && treeNode.right == null;
     }
 
     // Decodes your encoded data to tree.
@@ -91,7 +85,10 @@ public class Codec {
         TreeNode root = (TreeNode) list.get(0);
         deque.offer(root);
 
+        // 反序列化的时候: “儿子节点”作为“父节点”入队，为了在处理子节点的时候能够找到父亲节点。
         for (int i = 1; i < list.size(); i = i + 2) {
+            // i+2的原因：每个节点至多只有2个儿子，且即使儿子为null，null也被序列化了，所以这里要加2.
+            // 每个节点至多只有2个儿子，因此只要保证能把儿子处理完，父亲就可以出队了。
             Object parent = deque.pop();
             if (parent instanceof TreeNode) {
                 Object left = list.get(i);
@@ -169,6 +166,5 @@ public class Codec {
         TreeNode deserialize = deserialize(seial);
         serialize(deserialize);
 
-//        [1,2,3,4,5,6,7,8,null,null,9,10,null,null,null,null,null,null,null,11]
     }
 }
