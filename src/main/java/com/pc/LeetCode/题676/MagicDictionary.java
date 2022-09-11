c0350c6c57e01a2014f632d33a9d58b946d94072
EvanZhou666 <1043056891@qq.com>
2022-09-11 19:32:32 +0800

    feat:【前缀树系列】题676.实现一个魔法字典-CV大法

---
+++ src/main/java/com/pc/LeetCode/题676/MagicDictionary.java
@@ -0,0 +1,72 @@
+package com.pc.LeetCode.题676;
+
+/**
+ * 676. 实现一个魔法字典
+ * 设计一个使用单词列表进行初始化的数据结构，单词列表中的单词 互不相同 。 如果给出一个单词，请判定能否只将这个单词中一个字母换成另一个字母，使得所形成的新单词存在于你构建的字典中。
+ *
+ * 实现 MagicDictionary 类：
+ *
+ * MagicDictionary() 初始化对象
+ * void buildDict(String[] dictionary) 使用字符串数组 dictionary 设定该数据结构，dictionary 中的字符串互不相同
+ * bool search(String searchWord) 给定一个字符串 searchWord ，判定能否只将字符串中 一个 字母换成另一个字母，使得所形成的新字符串能够与字典中的任一字符串匹配。如果可以，返回 true ；否则，返回 false 。
+ * 题目的意思是一定要替换一次
+ */
+public class MagicDictionary {
+
+    Trie root;
+
+    public MagicDictionary() {
+        root = new Trie();
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String word : dictionary) {
+            Trie cur = root;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                int idx = ch - 'a';
+                if (cur.child[idx] == null) {
+                    cur.child[idx] = new Trie();
+                }
+                cur = cur.child[idx];
+            }
+            cur.isFinished = true;
+        }
+    }
+
+    public boolean search(String searchWord) {
+        return dfs(searchWord, root, 0, false);
+    }
+
+    private boolean dfs(String searchWord, Trie node, int pos, boolean modified) {
+        if (pos == searchWord.length()) {
+            return modified && node.isFinished;
+        }
+        int idx = searchWord.charAt(pos) - 'a';
+        if (node.child[idx] != null) {
+            if (dfs(searchWord, node.child[idx], pos + 1, modified)) {
+                return true;
+            }
+        }
+        if (!modified) {
+            for (int i = 0; i < 26; ++i) {
+                if (i != idx && node.child[i] != null) {
+                    if (dfs(searchWord, node.child[i], pos + 1, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+class Trie {
+    boolean isFinished;
+    Trie[] child;
+
+    public Trie() {
+        isFinished = false;
+        child = new Trie[26];
+    }
+}
