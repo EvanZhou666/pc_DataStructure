c4f908e860c51edff7807f8802efce643115ef84
EvanZhou666 <1043056891@qq.com>
2022-09-12 19:56:59 +0800

    feat:【二分搜索系列】

---
+++ src/main/java/com/pc/LeetCode/题278/Solution.java
@@ -0,0 +1,29 @@
+package com.pc.LeetCode.题278;
+
+/**
+ * 278. 第一个错误的版本
+ */
+public class Solution {
+
+    public int firstBadVersion(int n) {
+
+        int left = 0, right = n - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + ( right - left) / 2;
+
+            // [left,mid]
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    boolean isBadVersion(int version){
+        return false;
+    }
+
+}
