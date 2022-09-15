9fbdf0b933c9505337bbc8a4952ae1b22197382f
EvanZhou666 <1043056891@qq.com>
2022-09-16 00:11:28 +0800

    feat:【想不到系列】-题172.阶乘后的零

---
+++ src/main/java/com/pc/LeetCode/题172/Solution.java
@@ -0,0 +1,20 @@
+package com.pc.LeetCode.题172;
+
+/**
+ * 172. 阶乘后的零
+ * 给定一个整数 n ，返回 n! 结果中尾随零的数量。
+ *
+ * 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+ *
+ *
+ */
+public class Solution {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+}
