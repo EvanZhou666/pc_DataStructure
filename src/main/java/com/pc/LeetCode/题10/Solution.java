41f6a275268ec52fbc07a5093563a662fbe79ee6
EvanZhou666 <1043056891@qq.com>
2022-09-30 17:31:11 +0800

    feat:【动态规划系列】-题10.正则匹配

--- src/main/java/com/pc/LeetCode/题10/Solution.java
+++ src/main/java/com/pc/LeetCode/题10/Solution.java
@@ -5,61 +5,67 @@ import com.pc.LeetCode.common.Assert;
 /**
  * 10. 正则表达式匹配
  * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
- *
+ * <p>
  * '.' 匹配任意单个字符
  * '*' 匹配零个或多个前面的那一个元素
  * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
  */
 public class Solution {
 
+    /**
+     * 动态规划，就是边界条件太多
+     * @param s
+     * @param p
+     * @return
+     */
     public boolean isMatch(String s, String p) {
-        char[] chars = s.toCharArray();
-        char[] patterns = p.toCharArray();
+        char[] str = new char[s.length() + 1];
+        char[] patterns = new char[p.length() + 1];
+        System.arraycopy(s.toCharArray(), 0, str, 1, str.length - 1);
+        System.arraycopy(p.toCharArray(), 0, patterns, 1, patterns.length - 1);
 
-        int index = 0;
+        boolean[][] dp = new boolean[str.length][patterns.length];
+        dp[0][0] = true;
 
-        Character lastChar = null;
+        // i和j是针对dp数组的下标，对应到s,p的时候有1个偏移量
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
 
-        for (char pattern : patterns) {
+                // 如果是字母或者"."
+                if ((Character.isLetter(patterns[j])) || patterns[j] == '.') {
+                    // "."可以和任何字符匹配；如果是字母则必须相等； i >= 1 防止越界
+                    if ((patterns[j] == str[i] || patterns[j] == '.') && i >= 1) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                } else {
+                    boolean temp1 = false;
+                    boolean temp2 = false;
 
-           if (pattern == '.') {
-               if (index >= chars.length) {
-                   return false;
-               }
-               lastChar = '.';
-               index ++;
-           } else if (pattern == '*') {
-               while (index < chars.length) {
-                   if (lastChar == '.') {
-                       index ++;
+                    // 场景1：*前面的字符出现多次，patter[j-1...j]看作整体,多次匹配str，（条件是：str[i] 和 patterns[j-1]能够匹配）
+                    if ((str[i] == patterns[j - 1] || (patterns[j - 1] == '.' && str[i] != '\u0000')) && i >= 1) {
+                        temp1 = dp[i - 1][j];
+                    }
 
-                   } else {
-//                   上1个字符是字母，那么当前字符必须也是字母
-                       if (chars[index] != lastChar) {
-                           return false;
-                       }
-                       lastChar = chars[index];
-                       index ++;
-                   }
-               }
-               return true;
+                    // 场景2：patter[j-1...j]看作整体，不参与匹配。
+                    if (j >= 2) {
+                        temp2 = dp[i][j - 2];
+                    }
+                    // 场景1和场景2 有1个为真，则结果为真
+                    dp[i][j] = temp1 || temp2;
+                }
 
-           } else {
-               if (index >= chars.length || pattern != chars[index]) {
-                   return false;
-               }
-               lastChar = chars[index];
-               index++;
-           }
+            }
         }
-        return index >= chars.length;
+
+        return dp[s.length()][p.length()];
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        Assert.assertIsFalse(solution.isMatch("aa", "a"));
-//        Assert.assertIsTrue(solution.isMatch("aa", "a*"));
-//        Assert.assertIsTrue(solution.isMatch("ab", ".*"));
+        Assert.assertIsFalse(solution.isMatch("aa", "a"));
+        Assert.assertIsTrue(solution.isMatch("aa", "a*"));
+        Assert.assertIsTrue(solution.isMatch("ab", ".*"));
         Assert.assertIsTrue(solution.isMatch("aab", "c*a*b"));
+        Assert.assertIsFalse(solution.isMatch("bb", ".bab"));
     }
 }
